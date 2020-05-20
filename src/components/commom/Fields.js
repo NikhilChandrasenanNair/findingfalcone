d117b48851ffff50e75e4f70771573ba1b5fd1c1
Nikhil Chandrasenan Nair <Nikhil.c.nair@gmail.com>
2020-05-20 16:39:48 +0530

    Add files via upload

--- src/components/commom/Fields.js
+++ src/components/commom/Fields.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
 export class Button extends Component {
+    onClick(event) {
+        this.props.onClick(event);
+    }
+    
     render() {
         return (
-            <button>
-                
+            <button className={`btn ${this.props.className}`} onClick={this.onClick.bind(this)}>
+                Find Falcone
             </button>
         );
     }
 }
 
 export class Select extends Component {
-    constructor(props) {
-        super(props);        
-    }
+
     render() {
         let {options} = this.props;
         return (
@@ -35,9 +37,6 @@ export class Select extends Component {
 }
 
 export class DataList extends Component {
-    constructor(props) {
-        super(props);        
-    }
 
     onChange(event) {
         this.props.onChange(event);
@@ -60,8 +59,27 @@ export class DataList extends Component {
 
 export class RadioButtonGroup extends Component {
     constructor(props) {
-        super(props);        
+        super(props);
+
     }
+
+    // componentDidMount() {
+    //     if (this.props.stateData.selectedVehicleArray.length){
+    //         this.filterVehicle(this.props)
+    //     } 
+    // }    
+
+    // filterVehicle = (props) => {
+    //     let planetArray = [...props.options];
+    //     props.stateData.selectedVehicleArray.map((aVehicle) => {
+    //         planetArray.map((anItem, index) => {
+    //             if (anItem.name === aVehicle) {
+    //                 anItem.total_no = anItem.total_no - 1
+    //             }
+    //             return anItem
+    //         })
+    //     })
+    // }
 
     onChange(event) {
         this.props.onChange(event);
@@ -69,14 +87,15 @@ export class RadioButtonGroup extends Component {
 
     render() {
         let { options, name, showAdditionalData, stateData} = this.props;
+
         return (
             <div className={`field radio-grp`}>
                 <div className="values">
                     {options.map((aVehicle, index) => (
-                        <label key={index} className={`radio ${stateData[name].distance > aVehicle.max_distance ? 'disabled' : ''}`}>
-                            <input type="radio" value={aVehicle.name} name={name} onChange={this.onChange.bind(this)} disabled={stateData[name].distance > aVehicle.max_distance ? true : false}
+                        <label key={index} className={`radio ${((stateData[name].distance > aVehicle.max_distance) || aVehicle.total_no < 1) ? 'disabled' : ''}`}>
+                            <input type="radio" value={aVehicle.name} name={name} onChange={this.onChange.bind(this)} disabled={((stateData[name].distance > aVehicle.max_distance) || aVehicle.total_no < 1) ? true : false}
                                 data-max_distance={aVehicle.max_distance} data-speed={aVehicle.speed} data-total_no={aVehicle.total_no} />
-                            <span className={`vehicle-name ${stateData[name].distance > aVehicle.max_distance ? 'disabled' : ''}`}>{aVehicle.name} {!!(showAdditionalData) && <span>({aVehicle.total_no})</span> }</span>
+                            <span className={`vehicle-name ${((stateData[name].distance > aVehicle.max_distance) || aVehicle.total_no < 1) ? 'disabled' : ''}`}>{aVehicle.name} {!!(showAdditionalData) && <span>({aVehicle.total_no < 0 ? 0 : aVehicle.total_no})</span> }</span>
                         </label>
                     ))}
                 </div>
